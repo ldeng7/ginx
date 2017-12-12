0448c124f1f5c7b425f4fe9dcf9bca58e02057b6
Lynn Deng <beednln@163.com>
2017-12-12 17:44:24 +0800

    Update gorm_util.go

--- gorm_util/gorm_util.go
+++ gorm_util/gorm_util.go
@@ -2,14 +2,25 @@ package gorm_util
 
 import (
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-type GormError struct {
-	error
+type Hitable interface {
+	Hit()
 }
 
 type GormCtx struct {
-	Db *gorm.DB
+	Db      *gorm.DB
+	Hitable Hitable
+}
+
+func New(dsn string) (*GormCtx, error) {
+	g := &GormCtx{}
+	var err error
+	if g.Db, err = gorm.Open("mysql", dsn); nil != err {
+		return nil, err
+	}
+	return g, nil
 }
 
 func (g *GormCtx) New() *gorm.DB {
@@ -18,64 +29,70 @@ func (g *GormCtx) New() *gorm.DB {
 
 // CRUDs
 
-func outOrPanic(out *gorm.DB) *gorm.DB {
+func (g *GormCtx) outOrPanic(out *gorm.DB) *gorm.DB {
 	if nil != out.Error {
-		panic(GormError{out.Error})
+		if nil != g.Hitable {
+			g.Hitable.Hit()
+		}
+		panic(out.Error)
 	}
 	return out
 }
 
-func outOrNotfoundOrPanic(out *gorm.DB) *gorm.DB {
+func (g *GormCtx) outOrNotfoundOrPanic(out *gorm.DB) *gorm.DB {
 	if (nil != out.Error) && !out.RecordNotFound() {
-		panic(GormError{out.Error})
+		if nil != g.Hitable {
+			g.Hitable.Hit()
+		}
+		panic(out.Error)
 	}
 	return out
 }
 
 func (g *GormCtx) First(db *gorm.DB, value interface{}, where ...interface{}) *gorm.DB {
-	return outOrNotfoundOrPanic(db.First(value, where...))
+	return g.outOrNotfoundOrPanic(db.First(value, where...))
 }
 
 func (g *GormCtx) Last(db *gorm.DB, value interface{}, where ...interface{}) *gorm.DB {
-	return outOrNotfoundOrPanic(db.Last(value, where...))
+	return g.outOrNotfoundOrPanic(db.Last(value, where...))
 }
 
 func (g *GormCtx) Find(db *gorm.DB, value interface{}, where ...interface{}) *gorm.DB {
-	return outOrNotfoundOrPanic(db.Find(value, where...))
+	return g.outOrNotfoundOrPanic(db.Find(value, where...))
 }
 
 func (g *GormCtx) Count(db *gorm.DB, value interface{}) *gorm.DB {
-	return outOrPanic(db.Count(value))
+	return g.outOrPanic(db.Count(value))
 }
 
 func (g *GormCtx) Scan(db *gorm.DB, dest interface{}) *gorm.DB {
-	return outOrPanic(db.Scan(dest))
+	return g.outOrPanic(db.Scan(dest))
 }
 
 func (g *GormCtx) Create(db *gorm.DB, value interface{}) *gorm.DB {
-	return outOrPanic(db.Create(value))
+	return g.outOrPanic(db.Create(value))
 }
 
 func (g *GormCtx) Save(db *gorm.DB, value interface{}) *gorm.DB {
-	return outOrPanic(db.Save(value))
+	return g.outOrPanic(db.Save(value))
 }
 
 func (g *GormCtx) Update(db *gorm.DB, attrs ...interface{}) *gorm.DB {
-	return outOrPanic(db.Update(attrs...))
+	return g.outOrPanic(db.Update(attrs...))
 }
 
 func (g *GormCtx) Updates(db *gorm.DB, values interface{}, ignore ...bool) *gorm.DB {
-	return outOrPanic(db.Updates(values, ignore...))
+	return g.outOrPanic(db.Updates(values, ignore...))
 }
 
 func (g *GormCtx) UpdateColumn(db *gorm.DB, attrs ...interface{}) *gorm.DB {
-	return outOrPanic(db.UpdateColumn(attrs...))
+	return g.outOrPanic(db.UpdateColumn(attrs...))
 }
 
 func (g *GormCtx) UpdateColumns(db *gorm.DB, values interface{}) *gorm.DB {
-	return outOrPanic(db.UpdateColumns(values))
+	return g.outOrPanic(db.UpdateColumns(values))
 }
 
 func (g *GormCtx) Delete(db *gorm.DB, value interface{}, where ...interface{}) *gorm.DB {
-	return outOrPanic(db.Delete(value, where...))
+	return g.outOrPanic(db.Delete(value, where...))
 }
